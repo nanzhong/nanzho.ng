b0c33c0954246a6293415e187dc7e74f552e2cd1
Nan Zhong <me@nanzho.ng>
2021-08-17 22:45:49 -0400

    Switch to straight.el for managing packages

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
-FROM silex/emacs:27.1 AS build
+FROM silex/emacs:27.2 AS build
 ARG BASE_URL
 
-RUN curl -sL https://deb.nodesource.com/setup_15.x | bash -
-RUN apt-get install -y nodejs nginx
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash -
+RUN apt-get install -y nodejs nginx git
 RUN npm install -g sass
 
 WORKDIR /workspace
+
+COPY dependencies.el straight-versions.el /workspace/
+RUN emacs --script dependencies.el
+
 COPY . .
 RUN sass --style=compressed theme/static/scss:theme/static/css
 
-RUN mkdir -p output=
+RUN mkdir -p output
 RUN emacs --script publish.el
 RUN find posts -type f | grep -v .org | xargs -i cp '{}' 'output/{}'
 
--- dependencies.el
+++ dependencies.el
@@ -1,30 +1,52 @@
 ;;; dependencies.el --- Install and configure dependencies
 
-;; Setup package management
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(setq package-enable-at-startup nil)
 
-;; Install and configure dependencies
-;; (add-to-list 'load-path "../../emacs-love/weblorg")
-;; (require 'weblorg)
-(use-package weblorg :ensure t)
+;; Boostrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Configure straight package version lockfile
+(setq straight-profiles '((nil . "/workspace/straight-versions.el")))
+
+;; Bootstrap use-package
+(straight-use-package 'use-package)
+
+(use-package weblorg
+  :straight t)
 
 (use-package org
+  :straight t
   :config
   (setq org-src-preserve-indentation t))
 
 (use-package htmlize
-  :ensure t
+  :straight t
   :config
   (setq org-html-htmlize-output-type 'css))
-(use-package rainbow-delimiters :ensure t)
+(use-package rainbow-delimiters
+  :straight t)
 
 (use-package python
+  :straight t
   :config
   (setq python-indent-guess-indent-offset-verbose nil))
+
+(use-package fish-mode
+  :straight t)
+
+;; (straight-pull-all)
+;; (straight-freeze-versions)
+;; (straight-thaw-versions)
 
 ;;; dependencies.el ends here
